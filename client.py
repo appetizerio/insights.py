55a51851433eafda969ff6cff66b73f6e0b3a1de
Mingyuan Xia <ken.mingyuan@gmail.com>
2017-03-05 16:05:08 +0800

    fix English

--- client.py
+++ client.py
@@ -43,7 +43,7 @@ def rewrite(args):
     print('upload......')
     ret, info = put_file(token, key, args.apk)
     print(ret)
-    if (ret is None or 'code' not in ret or ret['code'] != 200):
+    if ret is None or 'code' not in ret or ret['code'] != 200:
         print('upload error')
         return 1
 
@@ -56,11 +56,11 @@ def rewrite(args):
             print(r_json)
             return 1
         if r_json['state'] == 'return_upload_auth' or r_json['state'] == 'upload_finish' or r_json['state'] == 'server_download':
-            print('waiting...... server download apk')
+            print('waiting...... server is downloading the APK')
         elif r_json['state'] == 'rewriting':
-            print('waiting...... server rewriting apk')
+            print('waiting...... server is rewriting the APK')
         elif r_json['state'] == 'rewrite_success':
-            print('waiting...... server uploading apk')
+            print('waiting...... server is uploading the rewritten APK')
         elif r_json['state'] == 'server_upload_success':
             print('Success !')
             break
@@ -75,9 +75,9 @@ def rewrite(args):
     print('3. download rewritten APK')
     r = requests.get(download_url)
     if r.status_code != 200:
-        print('download fail')
+        print('download failed')
         return 1
-    print('download complete')
+    print('download completed')
     with open(args.rewrited_apk, 'wb') as f:
         for chunk in r.iter_content(chunk_size=1024000):
             f.write(chunk)
@@ -86,20 +86,22 @@ def rewrite(args):
     for d in serialnos:
         subprocess.check_call(['adb', '-s', d, 'uninstall', pkg])
         subprocess.check_call(['adb', '-s', d, 'install', args.rewrited_apk])  # Note: Xiaomi will pop up a dialog
+    print('rewritten APK installed')
 
-    print('5. prepare permission')
+    print('5. grant permissions for logging')
     for d in serialnos:
         subprocess.check_call(['adb', '-s', d, 'shell', 'pm', 'grant', pkg, 'android.permission.WRITE_EXTERNAL_STORAGE'])
         subprocess.check_call(['adb', '-s', d, 'shell', 'pm', 'grant', pkg, 'android.permission.READ_EXTERNAL_STORAGE'])
-    print('write log permission complete')
-    print('ALL Success ! Test by your self now')
+    print('permission granted')
+    print('All done! Start testing now')
+
 
 def analyze(args):
     log_zip = args.pkg_name + '.log.zip'
     serialnos = args.serialnos.split(',')
     DEVICE_LOG = DEVICE_LOG_BASE + args.pkg_name + '.log'
     token = None
-    print('0. harvest logs from devices and zip')
+    print('0. harvest and compress device logs')
     with zipfile.ZipFile(log_zip, 'w') as myzip:
         for d in serialnos:
             subprocess.check_call(['adb', '-s', d, 'pull', DEVICE_LOG, d + '.log'])
@@ -116,17 +118,17 @@ def analyze(args):
     key = r_json['key']
 
 
-    print('2. upload log zip file')
-    print('pkg: ' + args.pkg_name)
-    print('zip file: ' + log_zip)
-    print('upload......')
+    print('2. upload log files')
+    print('package name: ' + args.pkg_name)
+    print('log file: ' + log_zip)
+    print('uploading......')
     ret, info = put_file(token, key, log_zip)
     if (ret is None or 'code' not in ret or ret['code'] != 200):
         print('upload error')
         return 1
 
 
-    print('3. wait for analyze to be processed')
+    print('3. server analyzing')
     r_json = None
     while True:
         r = requests.get(AUTH_BASE + 'api/v1/insight/client_query_analyze_state', params={'key': key})
@@ -135,29 +137,29 @@ def analyze(args):
             print(r_json)
             return 1
         if r_json['state'] == 'return_upload_auth' or r_json['state'] == 'upload_finish' or r_json['state'] == 'server_download':
-            print('waiting...... server download log')
+            print('waiting...... server is downloading the log')
         elif r_json['state'] == 'analyzing':
-            print('waiting...... server analyzing report')
+            print('waiting...... server is analyzing')
         elif r_json['state'] == 'analyze_success':
-            print('waiting...... server uploading report')
+            print('waiting...... server is uploading the report')
         elif r_json['state'] == 'server_upload_success':
             print('Success !')
             break
         else:
             print(r_json)
-            print('analyze fail')
+            print('analysis failed')
             return 1
         time.sleep(ANXIETY)
     download_url = r_json['download_url']
     print(download_url)
 
 
-    print('4. download insight report')
+    print('4. download report')
     r = requests.get(download_url)
     if r.status_code != 200:
-        print('download fail')
+        print('download failed')
         return 1
-    print('download complete')
+    print('download completeed')
     with open(args.report_path, 'wb') as f:
         for chunk in r.iter_content(chunk_size=1024000):
             f.write(chunk)
@@ -167,7 +169,7 @@ def analyze(args):
     for d in serialnos:
         os.remove(d + '.log')
 
-    print('ALL Sucess ! Report file at: ' + args.report_path)
+    print('All done! Report file stored at: ' + args.report_path)
 
 
 def pkgname(args):
@@ -181,23 +183,23 @@ def main():
     subparsers = parser.add_subparsers(help='sub-command help')
     
     rewrite_parser = subparsers.add_parser('rewrite', help='Inject appetizer into apk')
-    rewrite_parser.add_argument('username', action='store', help='Appetizer.io account username, register from https://appetizer.io/')
-    rewrite_parser.add_argument('password', action='store', help='Appetizer.io account password')
-    rewrite_parser.add_argument('apk', action='store', help='the path of apk to be rewrited')
-    rewrite_parser.add_argument('rewrited_apk', action='store', help='the path and file name for rewrited apk save to')
-    rewrite_parser.add_argument('serialnos', action='store', help='android devices\' serinal number, multi devices split with comma')
+    rewrite_parser.add_argument('username', action='store', help='AppetizerIO account username, register at https://www.appetizer.io/')
+    rewrite_parser.add_argument('password', action='store', help='AppetizerIO account password')
+    rewrite_parser.add_argument('apk', action='store', help='the path to the APK')
+    rewrite_parser.add_argument('rewrited_apk', action='store', help='the complete path to save the rewritten APK')
+    rewrite_parser.add_argument('serialnos', action='store', help='a list of Android devices to install the rewritten APK, devices identified by their serinal numbers, comma separated')
     rewrite_parser.set_defaults(func=rewrite)
 
-    analyze_parser = subparsers.add_parser('analyze', help='Analyze log from devices')
-    analyze_parser.add_argument('username', action='store', help='Appetizer.io account username, register from https://appetizer.io/')
-    analyze_parser.add_argument('password', action='store', help='Appetizer.io account password')
-    analyze_parser.add_argument('pkg_name', action='store', help='the android application package name')
-    analyze_parser.add_argument('report_path', action='store', help='the path for report.json save to')
-    analyze_parser.add_argument('serialnos', action='store', help='android devices\' serinal number, multi devices split with comma')
+    analyze_parser = subparsers.add_parser('analyze', help='Analyze device logs and generate diagnosis report')
+    analyze_parser.add_argument('username', action='store', help='AppetizerIO account username, register at https://www.appetizer.io/')
+    analyze_parser.add_argument('password', action='store', help='AppetizerIO account password')
+    analyze_parser.add_argument('pkg_name', action='store', help='the package name of the Android app')
+    analyze_parser.add_argument('report_path', action='store', help='the path to save the report')
+    rewrite_parser.add_argument('serialnos', action='store', help='a list of Android devices to install the rewritten APK, devices identified by their serinal numbers, comma separated')
     analyze_parser.set_defaults(func=analyze)
 
-    pkgname_parser = subparsers.add_parser('pkgname', help='Get package name from apk file')
-    pkgname_parser.add_argument('apk', action='store', help='the apk path')
+    pkgname_parser = subparsers.add_parser('pkgname', help='Get the package name of an APK file')
+    pkgname_parser.add_argument('apk', action='store', help='the path to the APK file')
     pkgname_parser.set_defaults(func=pkgname)
 
     args = parser.parse_args()
